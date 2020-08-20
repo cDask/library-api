4b6af48744b8829af84e18f98419ee731860a654
Daniel Ask <cdanielask@gmail.com>
2020-08-21 09:13:23 +1000

    Added Seeds

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,5 @@
 class Book < ApplicationRecord
   has_many :borrows, dependent: :destroy
+  has_many :books, through: :borrows
   validates :title, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,17 +7,30 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 puts "Destroying all records"
+Borrow.destroy_all
 Book.destroy_all
 User.destroy_all
 
 puts "Creating Test User"
-User.create(email: "test@user.com", password: "password")
+testUser = User.create(email: "test@user.com", password: "password")
 puts "test@user.com created"
+
+2.times do
+  book = Book.create(title: Faker::Book.title, author: Faker::Book.author)
+  Borrow.create(status: "Borrowed", user: testUser, book: book)
+end
+
+2.times do
+  puts "Seeding User"
+  user = User.create(email: Faker::Internet.email, password: Faker::Internet.password)
+  puts "user created"
+  
+  2.times do
+    book = Book.create(title: Faker::Book.title, author: Faker::Book.author)
+    Borrow.create(status: "Borrowed", user: user, book: book)
+  end
+end
 
 10.times do
   Book.create(title: Faker::Book.title, author: Faker::Book.author)
 end
-
-# 2.times do
-#   User.create(email: Faker::Internet.email, password: Faker::Internet.password)
-# end
